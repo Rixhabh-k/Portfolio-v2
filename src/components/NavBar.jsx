6f879e2189b5c250b148bf8be644641068b58aac
rixhabh-k <kharwarrishabh891@gmail.com>
2026-08-16 21:50:37 +0530

    Implement responsive mobile navigation in NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,63 @@
-import {Link} from 'react-router-dom'
-import { GrMenu } from "react-icons/gr";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { GrMenu, GrClose } from "react-icons/gr";
 
 const NavBar = () => {
-  return (
-    <div className="navbar" >
-        <div className="nav-content">
-            <div className="logo">
-            <h1>Rishabh Kharwar</h1>
-        </div>
-        <div className="nav-links">
-            <Link>Work</Link>
-            <Link>Stack</Link>
-            <Link>Contact</Link>
-            <Link className='hire-him'>Hire Him</Link>
-        </div>
-        <div className="menu">
-            <GrMenu />
-        </div>
-        </div>
-    </div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default NavBar
+  return (
+    <div className="navbar">
+      <div className="nav-content">
+
+        <div className="logo">
+          <h1>Rishabh Kharwar</h1>
+        </div>
+
+        {/* Desktop Navigation */}
+        <div className="nav-links">
+          <Link to="/work">Work</Link>
+          <Link to="/stack">Stack</Link>
+          <Link to="/contact">Contact</Link>
+          <Link className="hire-him" to="/contact">
+            Hire Him
+          </Link>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <button
+          className="menu"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle menu"
+        >
+          {menuOpen ? <GrClose /> : <GrMenu />}
+        </button>
+
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+        <Link to="/work" onClick={() => setMenuOpen(false)}>
+          Work
+        </Link>
+
+        <Link to="/stack" onClick={() => setMenuOpen(false)}>
+          Stack
+        </Link>
+
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>
+          Contact
+        </Link>
+
+        <Link
+          className="hire-him"
+          to="/contact"
+          onClick={() => setMenuOpen(false)}
+        >
+          Hire Him
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
